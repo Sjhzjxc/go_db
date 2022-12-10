6b9c91a9f1013e04bcb39d0db435cd3c8153c9ea
sjhzjxc <sjhzjxc@gmail.com>
2022-12-10 18:18:04 +0800

    修复writer无法给logger赋值的错

--- writer.go
+++ writer.go
@@ -6,25 +6,25 @@ import (
 )
 
 type ZapWriter struct {
-	logger *zap.SugaredLogger
+	Logger *zap.SugaredLogger
 }
 
 // 问题 1 infoStr([info])这些是可以改的 2 外部日志和数据库日志两者按最高的来显示
 func (w ZapWriter) Printf(format string, args ...interface{}) {
-	if w.logger == nil {
-		w.logger.Error("错误， 未设置logger")
+	if w.Logger == nil {
+		w.Logger.Error("错误， 未设置logger")
 		return
 	}
 	if strings.Contains(format, "[info]") {
-		w.logger.Infof(format, args)
+		w.Logger.Infof(format, args)
 		return
 	} else if strings.Contains(format, "[warn]") {
-		w.logger.Warnf(format, args)
+		w.Logger.Warnf(format, args)
 		return
 	} else if strings.Contains(format, "[error]") {
-		w.logger.Errorf(format, args)
+		w.Logger.Errorf(format, args)
 		return
 	} else {
-		w.logger.Infof(format, args)
+		w.Logger.Infof(format, args)
 	}
 }
